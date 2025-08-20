3fff85db5204a5de2d3b62288394dc48843687cf
laskalaska <kovtun65sergey@gmail.com>
2025-08-20 22:55:26 +0300

    updated hooks file to do screenshot on failure

---
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -0,0 +1,39 @@
+import { BeforeAll, Before, AfterAll, After } from "@cucumber/cucumber";
+import { setDefaultTimeout } from "@cucumber/cucumber"
+import { chromium } from "playwright";
+// const {chromium} = require "playwright";
+
+BeforeAll({timeout: 60 * 1000}, async () => {
+  global.browser = await chromium.launch({
+    headless: false,
+  }
+  )
+})
+
+AfterAll(async () => {
+  await global.browser.close();
+})
+
+Before(async (scenario) => {
+  global.context = await global.browser.newContext({
+    recordVideo: {
+      dir: "./reports/videos/" + scenario.pickle.name,
+    },
+    // recordHar: {
+    //   path: "./reports/hars/" + scenario.pickle.name + ".har",
+    // },
+    // recordNetwork: "on",
+  });
+  global.page = await global.context.newPage();
+})
+
+After(async (scenario) => {
+
+  const scenarioStatus = scenario.result?.status;
+  if (scenarioStatus === 'FAILED') {
+    await global.page.screenshot({
+      path: "./reports/screenshots/" + scenario.pickle.name + ".png",
+    });
+  }
+  await global.page.close();
+})

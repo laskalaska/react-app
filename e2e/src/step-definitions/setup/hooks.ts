a1ff0c36d465cba96ec8f7449e13d013a9de7e83
laskalaska <kovtun65sergey@gmail.com>
2025-08-26 00:59:26 +0300

    adjusting hooks according to cucumber world

--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -1,39 +1,31 @@
-import { BeforeAll, Before, AfterAll, After } from "@cucumber/cucumber";
-import { setDefaultTimeout } from "@cucumber/cucumber"
-import { chromium } from "playwright";
+import { Before, After, ITestCaseHookParameter } from "@cucumber/cucumber";
+import { ScenarioWorld } from "./world";
 // const {chromium} = require "playwright";
 
-BeforeAll({timeout: 60 * 1000}, async () => {
-  global.browser = await chromium.launch({
-    headless: false,
-  }
-  )
-})
+Before(async function (this: ScenarioWorld, scenario) {
+  console.log('Running cucumber scenario' + scenario.pickle.name);
 
-AfterAll(async () => {
-  await global.browser.close();
-})
-
-Before(async (scenario) => {
-  global.context = await global.browser.newContext({
+  const contextOptions = {
     recordVideo: {
       dir: "./reports/videos/" + scenario.pickle.name,
     },
-    // recordHar: {
-    //   path: "./reports/hars/" + scenario.pickle.name + ".har",
-    // },
-    // recordNetwork: "on",
-  });
-  global.page = await global.context.newPage();
+  };
+
+  const ready = await this.init(contextOptions);
+  return ready;
 })
 
-After(async (scenario) => {
+After(async function(this: ScenarioWorld, scenario) { // this: ScenarioWorld → only tells TypeScript what this should look like.
+  const {
+    screen: {page, browser}
+  } = this; // { screen: { browser } } = this → creates a local variable browser. “pull out the browser property from this.screen into a local variable named browser”
 
   const scenarioStatus = scenario.result?.status;
   if (scenarioStatus === 'FAILED') {
-    await global.page.screenshot({
+    await page.screenshot({
       path: "./reports/screenshots/" + scenario.pickle.name + ".png",
     });
   }
-  await global.page.close();
+  await browser.close(); // browser.close() works because browser is no longer on this — you pulled it out. Without destructuring, you'd need this.screen.browser.close().
+  return browser
 })
